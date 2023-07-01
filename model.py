77cb4a7d21bfc6e942a02ff8b7c27bf6be944ec4
Abdulloh <akramovabdullo2002@mail.com>
2023-07-01 11:11:44 +0500

    second algorithm example

---
+++ model.py
@@ -0,0 +1,28 @@
+import random
+            # Kompyuter son kiritadi siz shuni topishish kerak, va sizga yordam beradi agar son katta bo'lsa son katta deysi agar son kichik  bo'lsa son kichik deydi
+# x = list(range(1, 11))
+# y = random.choice(x)
+# while True:
+#     print(y)
+#     q = int(input("komp tanlagan soni toping>>> "))
+#     if q == y:
+#         print("siz tug'ri topdiz")
+#         break
+#     elif q < y:
+#         print('bu so kichik')
+#     elif q > y:
+#         print("bu son katta")
+
+        # Siz son kiritasiz compyuer topishi kerak va sizga yordam beradi agar son katta bo'lsa son katta deysi agar son kichik  bo'lsa son kichik deydi
+# g = list(range(1, 11))
+# y = random.choice(g)
+# while True:
+#     x = int(input('son kirit>>> '))
+#     if x == y:
+#         print("siz son topdiz")
+#         break
+#     elif x < y:
+#         print('bu so kichik')
+#     elif x > y:
+#         print("bu son katta")
+
